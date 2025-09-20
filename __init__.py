c69d32b9f1f1f3065bff4b4653e18174ad007530
waliwuao <1276672206@qq.com>
2025-09-20 20:43:26 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,23 @@
+# rcline/__init__.py
+from .cat import Cat
+from .line import Line
+from .map import Map
+from .circle import Circle
+from .polygon import Polygon
+from .entity import Entity
+from .collide import Collide
+from .colors import (
+    PRIMARY_COLOR, SECONDARY_COLOR, ACCENT_COLOR,
+    LIGHT_GRAY, MEDIUM_GRAY, DARK_GRAY, SIMPLE_CMAP, ENTITY_COLORS,
+    BACKGROUND_COLOR, GRID_MAJOR_COLOR, GRID_MINOR_COLOR
+)
+
+__version__ = "0.6.0"
+__author__ = "Wally Hao"
+__description__ = "A 2D simulation package for cat and line interaction with collision detection"
+__all__ = [
+    "Cat", "Line", "Map", "Entity", "Collide","Polygon", "Circle",
+    "PRIMARY_COLOR", "SECONDARY_COLOR", "ACCENT_COLOR",
+    "LIGHT_GRAY", "MEDIUM_GRAY", "DARK_GRAY", "SIMPLE_CMAP", "ENTITY_COLORS",
+    "BACKGROUND_COLOR", "GRID_MAJOR_COLOR", "GRID_MINOR_COLOR"
+]
